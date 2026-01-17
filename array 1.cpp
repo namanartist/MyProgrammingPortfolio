14d5ad7ef2017e8cf2c1ab01cdf37fd97b82e338
namanartist <namanlahariya@outlook.in>
2026-01-17 11:12:55 +0530

    Initialize array and correct loop for output
    Fix array initialization and loop bounds.

---
+++ array 1.cpp
@@ -0,0 +1,11 @@
+#include <iostream>
+using namespace std;
+int main (){
+    int x[6]={19,10,8};
+    cout<<"Array element are";
+    for (int i=0; i<8; i++)
+    {
+        cout<<x[i]<<"";
+    }
+    return 0;
+}
