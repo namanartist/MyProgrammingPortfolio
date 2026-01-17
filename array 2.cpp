e472710b531b05470a6b47b1cbd48d161e3e3d33
namanartist <namanlahariya@outlook.in>
2026-01-17 11:13:56 +0530

    Add program to input and display 5 numbers

---
+++ array 2.cpp
@@ -0,0 +1,24 @@
+#include <iostream>
+using namespace std;
+int main()
+{
+    int numbers[5];
+    cout << "Enter 5 numbers: " << endl;
+
+    // Store input from user to array
+    for (int i = 0; i < 5; ++i)
+    {
+        cin >> numbers[i];
+    }
+
+    cout << "The numbers are: ";
+
+    // Print array elements
+    for (int n = 0; n < 5; ++n)
+    {
+        cout << numbers[n] << " ";
+    }
+
+    cout << endl;
+    return 0;
+}
